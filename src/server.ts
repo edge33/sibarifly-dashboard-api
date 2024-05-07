2de5e210cd103099b764ed5631d24223ac904ea8
Francesco <francesco@efem.dev>
2024-05-07 19:26:15 +0200

    Added dockerfile

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import fastify, { FastifyReply } from 'fastify';
-import cors from '@fastify/cors';
+// import cors from '@fastify/cors';
 
 const app = fastify({ logger: true });
 
-await app.register(cors, {
-  origin: '*',
-  methods: ['POST', 'GET']
-});
+// await app.register(cors, {
+//   origin: '*',
+//   methods: ['POST', 'GET']
+// });
 
 app.get('/', function (_, reply: FastifyReply) {
   return reply
@@ -15,7 +15,7 @@ app.get('/', function (_, reply: FastifyReply) {
     .send({ hello: 'world' });
 });
 
-app.listen({ port: Number(process.env.PORT) || 8000 }, (err) => {
+app.listen({ port: Number(process.env.PORT) || 8000, host: '0.0.0.0' }, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
